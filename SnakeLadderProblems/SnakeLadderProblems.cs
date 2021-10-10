81efa8281a4efdbed0e780fcd993a97d6365abbb
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-10 19:22:51 +0530

    Game Played With Single Player

---
+++ SnakeLadderProblems/SnakeLadderProblems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeLadderProblems
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            //local variable
+            int startPosition = 0;
+            //display
+            Console.WriteLine("Player Currnt Position: "+startPosition);
+
+        }
+    }
+}
