8d833463ee46ac76ef2d65ec56533acd29a738d5
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-10 20:35:17 +0530

    The Player Roll The Die And Get Number

--- SnakeLadderProblems/SnakeLadderProblems.cs
+++ SnakeLadderProblems/SnakeLadderProblems.cs
@@ -4,18 +4,24 @@ namespace SnakeLadderProblems
 {
     class SnakeLadderGame
     {
-        static void Main(string[] args)
-        {
-          
-     
-                //Create object using Random predifined class
-                Random roll = new Random();
-                //using next() method to get output 1 to 6
-                int getNum = roll.Next(1, 7);
 
-                //player roll the die and get no between 1 to 6
-                Console.WriteLine("Die show the Number: " + getNum);
+        // create method to roll the die
+         void Roll()
+        {
+           // create object using Random Predefined Class
+            Random die = new Random();
+            // using next() method to get output 1 to 6
+            int getNum = die.Next(1, 7);
        
+            //player roll the die and get no between 1 to 6
+            Console.WriteLine("Die show the Number: " + getNum);
+
+        }
+        public static void Main(string[] args)
+        {
+        SnakeLadderGame snakeLadder = new SnakeLadderGame();
+        snakeLadder.Roll();
+        
         }
     }
 }
