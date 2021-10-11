6cc85f12e44b6ff7c06ac9ce49b2877175699776
swapnil-bagul <bagulswapnilp@gmail.com>
2021-10-11 14:37:55 +0530

    Repeat till the Player reach Winning position

--- SnakeLadderProblems/SnakeLadderProblems.cs
+++ SnakeLadderProblems/SnakeLadderProblems.cs
@@ -4,47 +4,106 @@ namespace SnakeLadderProblems
 {
     class SnakeLadderGame
     {
+        //constant variable
+        public const int WON = 100;
+        public const int START_POSITION = 0;
+
+        //static variable
+        static int totalNum = 0;
 
         // create method to roll the die
-         static void Roll()
+        static void Roll()
         {
-           // create object using Random Predefined Class
+            // create object using Random Predefined Class
             Random die = new Random();
             // using next() method to get output 1 to 6
             int getNum = die.Next(1, 7);
-       
+
             //player roll the die and get no between 1 to 6
-            Console.WriteLine("Die show the Number: " + getNum);
+             Console.WriteLine("Die show the Number: " + getNum);
 
-        }
-         static void Check()
-        {
-            Random check = new Random();
-            int input = check.Next(0, 3);
+            totalNum = totalNum + getNum;
+            Console.WriteLine("Current Position is: " + totalNum);
 
-            // switch case programing constructs
-            switch (input)
+            //if else programming construct
+            if (totalNum > WON)
             {
-                case 0 :
-                    Console.WriteLine("No play the Player Stays the same position");
-                    break;
+                totalNum = totalNum - getNum;
+                Console.WriteLine("Current Position is: " + totalNum);
+                SnakeLadderGame.Roll();
 
-                case 1:
-                    SnakeLadderGame.Roll();
-                    Console.WriteLine("Ladder the player moves ahead by the position received in the die");
-                    break;
-
-                default:
-                    SnakeLadderGame.Roll();
-                    Console.WriteLine("Snake the player moves behind by the position received in the die");
-                    break;
+            }
+            else if (totalNum == WON)
+            {
+                Console.WriteLine("reach 100");
             }
 
-        }
+            else if (totalNum <= WON)
+            {
+
+
+                switch (totalNum)
+                {
+                    case 20:
+                        Console.WriteLine("Ladder");
+                        totalNum += 10;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 35:
+                        Console.WriteLine("Ladder");
+                        totalNum += 25;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 64:
+                        Console.WriteLine("Ladder");
+                        totalNum += 24;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 72:
+                        Console.WriteLine("Ladder");
+                        totalNum += 10;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 22:
+                        Console.WriteLine("Snake");
+                        totalNum -= 10;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 40:
+                        Console.WriteLine("Snake");
+                        totalNum -= 15;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 62:
+                        Console.WriteLine("Snake");
+                        totalNum -= 24;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                    case 90:
+                        Console.WriteLine("Snake");
+                        totalNum -= 30;
+                        Console.WriteLine("Current Position is: " + totalNum);
+                        break;
+                   
+                }
+
+            }
+            else
+            {
+                SnakeLadderGame.Roll();
+            }
+
+        }       
+  
         public static void Main(string[] args)
         {
-            // call static method
-            SnakeLadderGame.Check();
+            Console.WriteLine("Start Position is {0}",START_POSITION );
+           
+            while(totalNum != WON )
+            {
+                //call static method
+                SnakeLadderGame.Roll();
+            }
            
         }
     }
